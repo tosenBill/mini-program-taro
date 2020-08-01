b0121c77e15d52f58ed186c1d9d7fd9186435d2c
shentong <shentong@meishubao.com>
2020-08-01 23:50:00 +0800

    init

--- src/app.tsx
+++ src/app.tsx
@@ -1,24 +1,33 @@
-import React, { Component } from "react";
-import { Provider } from "react-redux";
-import { createStore, combineReducers } from "redux";
+import React from "react";
+import { Provider } from "@tarojs/redux";
+import Taro, { Component } from "@tarojs/taro";
+import configStore from "./store";
+import Index from "./pages/index";
 import "./app.scss";
 
-const reducers = combineReducers({
-  thread: (state = {}, action) => {
-    if (action.type === "SET_CURRENT_THREAD") {
-      return Object.assign({}, state, action.thread);
-    }
-    return state;
-  },
-});
+// console.log(Provider);
 
-const store = createStore(reducers);
+// const reducers = combineReducers({
+//   thread: (state = {}, action) => {
+//     if (action.type === "SET_CURRENT_THREAD") {
+//       return Object.assign({}, state, action.thread);
+//     }
+//     return state;
+//   },
+// });
+
+const store = configStore();
 
 class App extends Component {
   render() {
     // this.props.children 是将要会渲染的页面
-    return <Provider store={store}>{this.props.children}</Provider>;
+    return (
+      <Provider store={store}>
+        <Index />
+      </Provider>
+    );
   }
 }
 
-export default App;
+Taro.render(<App />, document.getElementById("app"));
+// export default App;
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -47,54 +47,82 @@
 // };
 
 // export default Index;
-import Taro from "@tarojs/taro";
-import React from "react";
-import { View } from "@tarojs/components";
-import { ThreadList } from "../../components/thread_list";
-import api from "../../api";
+// import React from "react";
+// import Taro, { Component } from "@tarojs/taro";
+// import { View } from "@tarojs/components";
+// import { ThreadList } from "../../components/thread_list";
+// import api from "../../api";
 
-// interface Props {
-//   loading: boolean;
-//   threads: Array<Object>;
+// export default class Index extends Component {
+//   config = {
+//     navigationBarTitleText: "首页",
+//   };
+
+//   state = {
+//     loading: true,
+//     threads: [],
+//   };
+
+//   async componentDidMount() {
+//     try {
+//       const res = await Taro.request({
+//         url: api.getAllBanner(),
+//       });
+//       const {
+//         data: { payload },
+//       } = res;
+//       this.setState({
+//         threads: payload,
+//         loading: false,
+//       });
+//     } catch (error) {
+//       Taro.showToast({
+//         title: "载入远程数据错误",
+//       });
+//     }
+//   }
+
+//   render() {
+//     const { loading = false, threads = [] } = this.state;
+//     return (
+//       <View className="index">
+//         <ThreadList threads={threads} loading={loading} />
+//       </View>
+//     );
+//   }
 // }
 
-class Index extends React.Component {
-  config = {
+import Taro, { Component, Config } from "@tarojs/taro";
+import { View, Text } from "@tarojs/components";
+import "./index.scss";
+
+export default class Index extends Component {
+  componentWillMount() {}
+
+  componentDidMount() {}
+
+  componentWillUnmount() {}
+
+  componentDidShow() {}
+
+  componentDidHide() {}
+
+  /**
+   * 指定config的类型声明为: Taro.Config
+   *
+   * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
+   * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
+   * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
+   */
+  config: Config = {
     navigationBarTitleText: "首页",
   };
 
-  state = {
-    loading: true,
-    threads: [],
-  };
-
-  async componentDidMount() {
-    try {
-      const res = await Taro.request({
-        url: api.getAllBanner(),
-      });
-      const {
-        data: { payload },
-      } = res;
-      this.setState({
-        threads: payload,
-        loading: false,
-      });
-    } catch (error) {
-      Taro.showToast({
-        title: "载入远程数据错误",
-      });
-    }
-  }
-
   render() {
-    const { loading = false, threads = [] } = this.state;
     return (
       <View className="index">
-        <ThreadList threads={threads} loading={loading} />
+        <Text>Hello world!</Text>
       </View>
     );
   }
 }
-
-export default Index;
